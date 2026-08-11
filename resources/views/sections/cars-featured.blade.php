13833b714db02ec73b04f6177ea170bc7df05e0c
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2026-08-12 02:44:32 +0600

    fix: replace i font icons with clean SVG arrows and suppress Swiper font pseudo-elements to resolve dual overlaid arrows bug

--- resources/views/sections/cars-featured.blade.php
+++ resources/views/sections/cars-featured.blade.php
@@ -8,10 +8,14 @@
 			<div class="col-md-3 position-relative mb-30 wow fadeInUp">
 				<div class="box-button-slider box-button-slider-team justify-content-end">
 					<div class="swiper-button-prev swiper-button-prev-style-1 swiper-button-prev-2">
-						<i class="fi fi-rr-arrow-left"></i>
+						<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M10 3.5L4.5 8L10 12.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
 					</div>
 					<div class="swiper-button-next swiper-button-next-style-1 swiper-button-next-2">
-						<i class="fi fi-rr-arrow-right"></i>
+						<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M6 3.5L11.5 8L6 12.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
 					</div>
 				</div>
 			</div>
