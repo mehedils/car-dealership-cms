0b70de6b76bbf398fc07a1b044245f0fac91a04f
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2026-08-12 02:31:14 +0600

    fix: vertical alignment for Show All Brands arrow and brand theme color integration

--- resources/views/sections/brand.blade.php
+++ resources/views/sections/brand.blade.php
@@ -2,11 +2,11 @@
 	<div class="container">
 		<div class="box-search-category">
 			<h3 class="heading-3 mb-0 neutral-1000 wow fadeInUp">Premium Brands</h3>
-			<div class="d-flex align-items-center justify-content-between">
-				<p class="text-lg-medium neutral-500 wow fadeInUp">Unveil the Finest Selection of High-End Vehicles</p>
-				<a href="{{ url('/cars') }}" class="text-sm-bold neutral-1000 wow fadeInUp">
-					Show All Brands
-					<i class="fi fi-rr-arrow-right ms-1"></i>
+			<div class="d-flex align-items-center justify-content-between mb-4">
+				<p class="text-lg-medium neutral-500 mb-0 wow fadeInUp">Unveil the Finest Selection of High-End Vehicles</p>
+				<a href="{{ url('/cars') }}" class="text-sm-bold neutral-1000 d-inline-flex align-items-center gap-2 link-hover-primary wow fadeInUp">
+					<span>Show All Brands</span>
+					<i class="fi fi-rr-arrow-right text-primary fs-6"></i>
 				</a>
 			</div>
 			<div class="carouselTicker carouselTicker-left box-list-brand-car justify-content-center wow fadeIn">
