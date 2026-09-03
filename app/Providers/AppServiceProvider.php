1567a2c314118f0ecfa3154713d6a4a14b6f618e
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2026-09-04 00:42:43 +0600

    fix: send password reset emails immediately without queue dependency

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,18 @@ class AppServiceProvider extends ServiceProvider
         if (file_exists(app_path('helpers.php'))) {
             require_once app_path('helpers.php');
         }
+
+        // Ensure Filament password reset emails send immediately without requiring a queue worker
+        $this->app->bind(\Filament\Notifications\Auth\ResetPassword::class, function ($app, array $parameters) {
+            return new class($parameters['token']) extends \Illuminate\Auth\Notifications\ResetPassword {
+                public string $url;
+
+                protected function resetUrl($notifiable): string
+                {
+                    return $this->url;
+                }
+            };
+        });
     }
 
     /**
